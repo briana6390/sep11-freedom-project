338ebcaceb9492cf71035c20d3dcb1deed71dddf
briana6390 <114589855+briana6390@users.noreply.github.com>
2024-04-30 22:04:55 +0000

    Tue, Apr 30, 2024, 6:04 PM -04:00

--- mygame/src/main.js
+++ mygame/src/main.js
@@ -1,6 +1,9 @@
-import kaboom from "kaboom"
+import kaboom from "https://unpkg.com/kaboom@3000.0.1/dist/kaboom.mjs";
 
-kaboom(152,176,0)
+kaboom({
+    background: [162,183,0]
+});
+
     // Blue Ship
       loadRoot("sprites/");
       loadSpriteAtlas("blueship.png",{
