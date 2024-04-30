d677ab6bf15adfb09ab7e9212204e515f9615804
briana6390 <114589855+briana6390@users.noreply.github.com>
2024-04-30 21:58:07 +0000

    Tue, Apr 30, 2024, 5:58 PM -04:00

--- mygame/src/main.js
+++ mygame/src/main.js
@@ -1,32 +1,69 @@
-const PLAYER_MOVE_SPEED = 500;
-const SCREEN_EDGE = 100;
+import kaboom from "kaboom"
+    // Blue Ship
+      loadRoot("sprites/");
+      loadSpriteAtlas("blueship.png",{
+          "ship":{
+              "x":0,
+              "y":0,
+              "width":180,
+              "height": 30,
+              "sliceX":3,
+              "sliceY":1,
+          }
+      });
+
+      //Red
+      loadRoot("sprites/");
+      loadSpriteAtlas("red.png",{
+          "red":{
+              "x":0,
+              "y":0,
+              "width":30,
+              "height": 30,
+
+          }
+      });
+
+      scene("game",() => {
+          const ship = add([
+              sprite("blueship"),
+              scale(1),
+              orgin("center"),
+              pos(500, 500),
+              area(),
+              {
+                  score: 100,
+              },
+          ])
+          "player"
+      });
+
+
+
+      scene("gameOver", (score) =>{
+
+      });
 
 
 
 
 
-    const player = add([
-        sprite(blueship),
-        scale(0.17)
-        pos(width()/2, height()/2),
-        area(),
-        "player"
-    ]);
 
 
 
 
 
 
-    onKeyDown("left", () => {
-    if(blueship.pos.x <= width() - SCREEN_EDGE){
-		blueship.move(PLAYER_MOVE_SPEED,0)
-	}
-    })
 
-    onKeyDown("right", () => {
-    blueship.move(SPEED, 0)
-    })
+    // onKeyDown("left", () => {
+    // if(blueship.pos.x <= width() - SCREEN_EDGE){
+	// 	blueship.move(PLAYER_MOVE_SPEED,0)
+	// }
+    // })
+
+    // onKeyDown("right", () => {
+    // blueship.move(SPEED, 0)
+    // })
 
 
 
