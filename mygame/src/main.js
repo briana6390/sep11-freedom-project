56749b6ac2f1f354dfb1ca2a1ef020b47eb0463f
briana6390 <114589855+briana6390@users.noreply.github.com>
2024-05-01 10:47:01 +0000

    Wed, May 1, 2024, 6:47 AM -04:00

--- mygame/src/main.js
+++ mygame/src/main.js
@@ -1,46 +1,72 @@
-import kaboom from "kaboom";
+// import kaboom from "kaboom";
 
 
 
-    // Blue Ship
-      loadRoot("sprites/");
-      loadSpriteAtlas("blueship.png",{
-          "ship":{
-              "x":0,
-              "y":0,
-              "width":180,
-              "height": 30,
-              "sliceX":3,
-              "sliceY":1,
-          }
-      });
+//     // Blue Ship
+//       loadRoot("sprites/");
+//       loadSpriteAtlas("blueship.png",{
+//           "ship":{
+//               "x":0,
+//               "y":0,
+//               "width":180,
+//               "height": 30,
+//               "sliceX":3,
+//               "sliceY":1,
+//           }
+//       });
 
-      //Red
-      loadRoot("sprites/");
-      loadSpriteAtlas("red.png",{
-          "red":{
-              "x":0,
-              "y":0,
-              "width":30,
-              "height": 30,
+//       //Red
+//       loadRoot("sprites/");
+//       loadSpriteAtlas("red.png",{
+//           "red":{
+//               "x":0,
+//               "y":0,
+//               "width":30,
+//               "height": 30,
 
-          }
-      });
+//           }
+//       });
 
-      //BlueShip
-      add([
-        sprite("ship"),
-    ])
+//       //BlueShip
+//       add([
+//         sprite("ship"),
+//     ])
 
-    const player = add([
-        sprite("ship")
-    ]);
-
-
+//     const player = add([
+//         sprite("ship")
+//     ]);
 
 
 
 
+// Initialize Kaboom.js
+kaboom();
+
+// Define constants
+const PLAYER_SPEED = 200;
+const ALIEN_SPEED = 50;
+const ALIEN_FREQUENCY = 0.5;
+const BULLET_SPEED = 400;
+
+// Load assets
+loadRoot('https://i.imgur.com/');
+loadSprite('ship', 'blueship.png');
+loadSprite('red', 'red.png');
+
+// Define player
+const player = add([
+  sprite('ship'),
+  pos(width() / 2, height() - 40),
+  origin('center')
+]);
+
+// Player movement
+keyDown('left', () => {
+  player.move(-PLAYER_SPEED, 0);
+});
+keyDown('right', () => {
+  player.move(PLAYER_SPEED, 0);
+});
 
 
 
